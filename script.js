4caa06dd44a7c5650cd2c9498345a622dc28a0e2
DavidSaj <139820288+DavidSaj@users.noreply.github.com>
2023-11-14 15:44:28 +0100

    update website
    updated the picture on the side. js code including.
    
    now to finish add picture with 50px length and and same distance between the 3 pictures and on the sides with flex.

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const image = document.getElementById('scrollImage');
 let imagePosition = 0;
-
 function handleScroll() {
   const container = document.documentElement;
- 
   imagePosition = container.scrollTop;
-
   image.style.transform = `translateY(-${imagePosition}px)`;
 }
-
-
-
 window.addEventListener('scroll', handleScroll);
+
+const image2 = document.getElementById('scrollImage2');
+let imagePosition2 = 0;
+function handleScroll2() {
+  const container2 = document.documentElement;
+  imagePosition2 = container2.scrollTop;
+  image2.style.transform = `translateY(${imagePosition2}px)`;
+}
+window.addEventListener('scroll', handleScroll2);
+
+const image3 = document.getElementById('scrollImage3');
+let imagePosition3 = 0;
+function handleScroll3() {
+  const container3 = document.documentElement;
+  imagePosition3 = 0.5 * container3.scrollTop;
+  image3.style.transform = `translateY(-${imagePosition3}px)`;
+}
+window.addEventListener('scroll', handleScroll3);
